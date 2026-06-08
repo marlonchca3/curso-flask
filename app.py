bbb1bd36e6784b1c506699bd02be43d1895f8240
marlonchca3 <marlonchca3@gmail.com>
2026-06-07 20:28:17 -0500

    qwe

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, request
+from flask import Flask, flash, redirect, render_template, request, url_for
 
 from config import Config
-from models import db
+from models import Note, db
 from notes.routes import notes_bp
 from auth.routes import auth_bp
 
@@ -13,6 +13,9 @@ def create_app(config_class=Config):
     app.register_blueprint(notes_bp)
     app.register_blueprint(auth_bp)
 
+    with app.app_context():
+        db.create_all()
+
     @app.route("/acerca-de")
     def about():
         return "Esto es una app de notas"
@@ -22,5 +25,27 @@ def create_app(config_class=Config):
         if request.method == "POST":
             return "Formulario enviado correctamente", 201
         return "Pagina de contacto"
+
+    @app.route("/crear-nota", methods=["GET", "POST"])
+    def create_note():
+        if request.method == "POST":
+            title = request.form.get("title", "")
+            content = request.form.get("content", "")
+
+            if not len(title.strip()) > 10:
+                flash("El título es muy corto, minimo 10", "error")
+                return render_template("note_form.html")
+
+            if not len(content.strip()) > 20:
+                flash("El contenido es muy corto, minimo 20", "error")
+                return render_template("note_form.html")
+
+            note_db = Note(title=title, content=content)
+            db.session.add(note_db)
+            db.session.commit()
+            flash("Nota creada", "success")
+            return redirect(url_for("notes.home"))
+
+        return render_template("note_form.html")
 
     return app
--- notes/routes.py
+++ notes/routes.py
@@ -14,28 +14,6 @@ def home():
     return render_template("home.html", notes=notes)
 
 
-@notes_bp.route("/crear-nota", methods=["GET", "POST"])
-def create_note():
-    if request.method == "POST":
-        title = request.form.get("title", "")
-        content = request.form.get("content", "")
-
-        if not len(title.strip()) > 10:
-            flash("El título es muy corto, minimo 10", "error")
-            return render_template("note_form.html")
-
-        if not len(content.strip()) > 20:
-            flash("El título es muy corto, minimo 300", "error")
-            return render_template("note_form.html")
-
-        note_db = Note(title=title, content=content)
-        db.session.add(note_db)
-        db.session.commit()
-        flash("Nota creada", "success")
-        return redirect(url_for("notes.home"))
-    return render_template("note_form.html")
-
-
 @notes_bp.route("/editar-nota/<int:id>", methods=["GET", "POST"])
 def edit_note(id):
     note = Note.query.get_or_404(id)
